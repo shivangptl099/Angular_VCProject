99407d5e24ad48d531dddf57ef86fe80ee156328
Shivang <shivangptl099@gmail.com>
2024-07-04 14:55:17 +0530

    Dashboard Complete with Game Images

--- GameStoreApp/src/app/pages/dashboard/dashboard.component.ts
+++ GameStoreApp/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  
+  cart: string[] = [];
+
+  addToCart(gameName: string) {
+    this.cart.push(gameName);
+  }
+  removeFromCart(gameName: string){
+    let index =  this.cart.indexOf(gameName);
+    this.cart.splice(index,1);
+  }
+  placeOrder(){
+    if(this.cart.length == 0) alert("No items in Cart.");
+    else{
+      alert("Order Placed !");
+      this.cart.length = 0;
+    }
+  }
 }
